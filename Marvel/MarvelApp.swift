9108c7d169e01b90a680cd30f214a5d98f9c6183
AlexanderZbirnik <alxzorg@gmail.com>
2023-01-24 18:38:02 +0100

    [Create] Xcode project

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Alexander Zbirnik on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
