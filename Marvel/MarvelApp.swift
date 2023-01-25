51389ec53c1c0926fa328c34524e4559240a2ad3
AlexanderZbirnik <alxzorg@gmail.com>
2023-01-25 19:13:14 +0100

    [Update] git ignore file

--- Marvel/MarvelApp.swift
+++ Marvel/MarvelApp.swift
@@ -7,8 +7,21 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions
+        options: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        return true
+    }
+}
+
+
 @main
 struct MarvelApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
