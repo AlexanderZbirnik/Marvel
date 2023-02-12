7de57dca6934fe8db6aa1d2e06c7e291e5679308
AlexanderZbirnik <alxzorg@gmail.com>
2023-02-12 15:47:46 +0100

    [Refactor] series item reducer

--- Feature/Sources/Series/SeriesItemView/SeriesItemReducer.swift
+++ Feature/Sources/Series/SeriesItemView/SeriesItemReducer.swift
@@ -7,35 +7,30 @@ public struct SeriesItemReducer: ReducerProtocol {
     public struct State: Equatable, Identifiable {
         public var id: Series.Id
         var title = ""
-        var years = ""
         var imageUrl: URL
         
         public init(_ series: Series) {
             self.id = series.id
             self.title = series.title ?? ""
-            var years = ""
-            if let startYear = series.startYear {
-                years = "\(startYear)"
-            }
-            if let endYear = series.endYear, endYear != (series.startYear ?? 0) {
-                years += endYear == 2099 ? " - ..." : " - \(endYear)"
-            }
-            self.years = years
-            if var path = series.thumbnail?.path {
+            self.imageUrl = Self.parseThumbnail(series.thumbnail)
+        }
+        
+        static func parseThumbnail(_ image: MImage?) -> URL {
+            if var path = image?.path {
                 path = path.replacingOccurrences(of: "http:", with: "https:")
                 path += "/standard_medium"
-                if let ext = series.thumbnail?.extension {
+                if let ext = image?.extension {
                     path += ("." + ext)
                 } else {
                     path += ".jpg"
                 }
                 if let url = URL(string: path) {
-                    self.imageUrl = url
+                    return url
                 } else {
-                    self.imageUrl = URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_medium.jpg")!
+                    return URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_medium.jpg")!
                 }
             } else {
-                self.imageUrl = URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_medium.jpg")!
+                return URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_medium.jpg")!
             }
         }
     }
